6eee87f2a780fef10db6cfb6d8473b87044e0057
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-11-29 19:35:54 +0100

    Remove redundant game methods

--- src/game.js
+++ src/game.js
@@ -24,42 +24,6 @@ class Game {
 
     }
 
-    gameCheckForVictory() {
-        return this.opponent.board.checkEndGame()
-    }
-
-    attack(y, x) {
-
-        const attackAction = this.opponent.board.receiveAttack(y, x)
-
-        if (attackAction === "You already fired at these coordinates!") {
-            return attackAction;
-        } else if (attackAction === "HIT" && this.gameCheckForVictory()) {
-            return  `HIT! ${this.currentPlayer.name} WON!`
-        } else {
-            if (this.opponent === this.playerTwo) {
-                this.opponent = this.playerOne
-                this.currentPlayer = this.playerTwo
-            } else {
-                this.opponent = this.playerTwo
-                this.currentPlayer = this.playerOne
-            }
-            return attackAction
-        }  
-
-    }
-
-    aiAttack() {
-        const aittack = this.currentPlayer.aiMove(this.opponent.board);
-        if (aittack === "HIT" && this.gameCheckForVictory()) {
-            return  `HIT! ${this.currentPlayer.name} WON!`
-        } else {
-            this.currentPlayer = this.playerOne
-            this.opponent = this.playerTwo   
-            return aittack
-        }
-    }
-
 }
 
 export { Game }
